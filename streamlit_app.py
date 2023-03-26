8a409c42473309004aeda42b60e18ffb69f32f6b
beanrain35 <124814162+beanrain35@users.noreply.github.com>
2023-03-26 10:55:45 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.head('Breakfast Me
+streamlit.head('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Hard boiled free range egg')
