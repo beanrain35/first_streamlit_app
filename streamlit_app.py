98a2f630f1d215b676031ed0ed4b4d874c262226
beanrain35 <124814162+beanrain35@users.noreply.github.com>
2023-03-27 22:52:17 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,7 +74,7 @@ my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.text("The fruit load list contrains:")
-streamlit.text(my_data_row)
+streamlit.dataframe(my_data_row)
 
 streamlit.stop()
 
